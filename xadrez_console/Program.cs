17defdcb20aba7546d64222b5865b2b238841690
Luan Barleze <Luanbt63@gmail.com>
2022-07-02 18:35:33 -0300

    Comecando a implentar metodo de tabuleiro

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more informatio
 using tabuleiro;
+using xadrez_console;
 
 Tabuleiro tab = new Tabuleiro(8,8);
 
+Tela.imprimirTabuleiro(tab);
 
-Console.WriteLine(tab);
+Console.ReadLine();
--- xadrez_console/Tabuleiro/Tabuleiro.cs
+++ xadrez_console/Tabuleiro/Tabuleiro.cs
@@ -20,5 +20,13 @@ namespace tabuleiro
             pecas = new Peca[Linhas,Colunas];
         }
 
+
+        public Peca peca(int linha,int coluna)
+        {
+            return pecas[linha,coluna];
+        }
+
+
+
     }
 }
